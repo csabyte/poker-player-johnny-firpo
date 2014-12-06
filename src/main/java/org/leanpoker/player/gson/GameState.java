83160384b2c988dcaa19fa034c9ed1443fff9cfa
Dojcsák Sándor <dojcsak@astron.hu>
2014-12-06 11:15:14 +0100

    GameState olvasása.

---
+++ src/main/java/org/leanpoker/player/gson/GameState.java
@@ -0,0 +1,99 @@
+package org.leanpoker.player.gson;
+
+import java.util.List;
+import com.google.gson.annotations.SerializedName;
+
+public class GameState {
+
+    private List<Player> players;
+
+    private int smallBlind;
+
+    private int orbits;
+
+    private int dealer;
+
+    @SerializedName("community_cards")
+    private List<Card> communityCards;
+
+    private int currentBuyIn;
+
+    private int pot;
+
+    private int inAction;
+
+    private int minimumRaise;
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public int getSmallBlind() {
+        return smallBlind;
+    }
+
+    public void setSmallBlind(int smallBlind) {
+        this.smallBlind = smallBlind;
+    }
+
+    public int getOrbits() {
+        return orbits;
+    }
+
+    public void setOrbits(int orbits) {
+        this.orbits = orbits;
+    }
+
+    public int getDealer() {
+        return dealer;
+    }
+
+    public void setDealer(int dealer) {
+        this.dealer = dealer;
+    }
+
+    public List<Card> getCommunityCards() {
+        return communityCards;
+    }
+
+    public void setCommunityCards(List<Card> communityCards) {
+        this.communityCards = communityCards;
+    }
+
+    public int getCurrentBuyIn() {
+        return currentBuyIn;
+    }
+
+    public void setCurrentBuyIn(int currentBuyIn) {
+        this.currentBuyIn = currentBuyIn;
+    }
+
+    public int getPot() {
+        return pot;
+    }
+
+    public void setPot(int pot) {
+        this.pot = pot;
+    }
+
+    public int getInAction() {
+        return inAction;
+    }
+
+    public void setInAction(int inAction) {
+        this.inAction = inAction;
+    }
+
+    public int getMinimumRaise() {
+        return minimumRaise;
+    }
+
+    public void setMinimumRaise(int minimumRaise) {
+        this.minimumRaise = minimumRaise;
+    }
+
+}
---
+++ src/main/java/org/leanpoker/player/gson/Player.java
@@ -0,0 +1,79 @@
+package org.leanpoker.player.gson;
+
+import java.util.List;
+import com.google.gson.annotations.SerializedName;
+
+public class Player {
+
+    private String name;
+
+    private int stack;
+
+    private String status;
+
+    private int bet;
+
+    @SerializedName("hole_cards")
+    private List<Card> holeCards;
+
+    private String version;
+
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStack() {
+        return stack;
+    }
+
+    public void setStack(int stack) {
+        this.stack = stack;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getBet() {
+        return bet;
+    }
+
+    public void setBet(int bet) {
+        this.bet = bet;
+    }
+
+    public List<Card> getHoleCards() {
+        return holeCards;
+    }
+
+    public void setHoleCards(List<Card> holeCards) {
+        this.holeCards = holeCards;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
