ce29c9bcb6849c8510e05b47411f6979790f4e49
Dojcsák Sándor <dojcsak@astron.hu>
2014-12-06 11:20:45 +0100

    Emelés 100-al.

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,14 +1,22 @@
 package org.leanpoker.player;
 
+import org.leanpoker.player.gson.GameState;
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
-
-import java.util.Map;
 
 public class Player {
 
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
+        Gson gson = new Gson();
+        gson.fromJson(request, GameState.class);
+
+        return 100;
+        // return allIn();
+    }
+
+    public static int allIn() {
         return 1000;
     }
 
